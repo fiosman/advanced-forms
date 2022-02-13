40c83deae26094f631482f6e939e9156a421e51b
fiosman <fares.osman@outlook.com>
2022-02-12 23:32:40 -0500

    Adds mcq to sample survey

--- aa-forms/src/pages/SampleSurvey.js
+++ aa-forms/src/pages/SampleSurvey.js
@@ -3,10 +3,44 @@ import { Link } from "react-router-dom";
 import sample from "../_assets/sample.json";
 
 const SampleSurvey = () => {
+  const [reasonOption, setReasonOption] = useState();
+
+  const Questions = () => {
+    const questions = sample.questions;
+    return (
+      <ul>
+        <li>
+          <p>{questions[0].stem}</p>
+          {questions[0].options.map((option) => {
+            return (
+              <>
+                <input
+                  type="radio"
+                  checked={reasonOption === option.value}
+                  onChange={() => setReasonOption(option.value)}
+                  value={option.value}
+                  name={option.value}
+                />
+                <label>{option.text}</label>
+              </>
+            );
+          })}
+        </li>
+      </ul>
+    );
+  };
   return (
     <div>
-      <h3>Sample Survey Questionaire</h3>
+      <h2>
+        {sample.name} {sample.description}
+      </h2>
+      <p>{sample.instructions}</p>
+      <Questions />
       <Link to="/">Home </Link>
+
+      <form>
+        <h3></h3>
+      </form>
     </div>
   );
 };
