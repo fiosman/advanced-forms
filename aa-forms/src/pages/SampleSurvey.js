8e75b2c6a200cf0b292ad234613860886be6d8d6
fiosman <fares.osman@outlook.com>
2022-02-13 23:46:42 -0500

    adds another mc question

--- aa-forms/src/pages/SampleSurvey.js
+++ aa-forms/src/pages/SampleSurvey.js
@@ -4,20 +4,38 @@ import sample from "../_assets/sample.json";
 
 const SampleSurvey = () => {
   const [reasonOption, setReasonOption] = useState();
-
+  const [planningUseOption, setPlanningUseOption] = useState();
   const Questions = () => {
     const questions = sample.questions;
+    console.log(sample);
     return (
       <ul>
         <li>
           <p>{questions[0].stem}</p>
-          {questions[0].options.map((option) => {
+          {questions[0].options.map((option, idx) => {
             return (
               <>
                 <input
                   type="radio"
                   checked={reasonOption === option.value}
                   onChange={() => setReasonOption(option.value)}
+                  value={option.value}
+                  name={option.value}
+                />
+                <label>{option.text}</label>
+              </>
+            );
+          })}
+        </li>
+        <li>
+          <p>{questions[1].stem}</p>
+          {questions[1].options.map((option, idx) => {
+            return (
+              <>
+                <input
+                  type="radio"
+                  checked={planningUseOption === option.value}
+                  onChange={() => setPlanningUseOption(option.value)}
                   value={option.value}
                   name={option.value}
                 />
@@ -37,7 +55,6 @@ const SampleSurvey = () => {
       <p>{sample.instructions}</p>
       <Questions />
       <Link to="/">Home </Link>
-
       <form>
         <h3></h3>
       </form>
