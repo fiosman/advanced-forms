1536ec1752563707a54ef441b90d8c6c698918b3
fiosman <fares.osman@outlook.com>
2022-02-18 23:33:59 -0500

    Adds instructions for form

--- aa-forms/src/pages/SensoryPreferencesSurvey.js
+++ aa-forms/src/pages/SensoryPreferencesSurvey.js
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 import spi from "../_assets/spi.json";
 
 const SensoryPreferencesSurvey = () => {
+  console.log(spi);
+  const buildQuestions = () => {};
   return (
     <div>
       <div>
-        <h3>Sensory Preferences Survey</h3>
         <Link to="/">Home </Link>
+        <h3>Sensory Preferences Survey</h3>
+        <p>{spi.questions[0].instructions}</p>
       </div>
     </div>
   );
