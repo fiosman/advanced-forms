613801238fac2b3f746ddc0365e700cf626a078f
fiosman <fares.osman@outlook.com>
2022-02-20 23:52:03 -0500

    Set up sensory survey form

--- aa-forms/src/pages/SensoryPreferencesSurvey.js
+++ aa-forms/src/pages/SensoryPreferencesSurvey.js
@@ -5,13 +5,46 @@ import spi from "../_assets/spi.json";
 const SensoryPreferencesSurvey = () => {
   const questions = spi.questions.slice(1);
 
-  // const buildQuestions = () => {};
+  const buildQuestions = () => {
+    return (
+      <ul>
+        {questions.map((question) => {
+          return (
+            <li>
+              <p>{question.stem}</p>
+              <label>
+                <input type="radio"></input>
+                Strongly Agree
+              </label>
+              <label>
+                <input type="radio"></input>
+                Agree
+              </label>
+              <label>
+                <input type="radio"></input>
+                Netural
+              </label>
+              <label>
+                <input type="radio"></input>
+                Disagree
+              </label>
+              <label>
+                <input type="radio"></input>
+                Strongly Disagree
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
   return (
     <div>
       <div>
         <Link to="/">Home </Link>
         <h3>Sensory Preferences Survey</h3>
         <p>{spi.questions[0].instructions}</p>
+        {buildQuestions()}
       </div>
     </div>
   );
