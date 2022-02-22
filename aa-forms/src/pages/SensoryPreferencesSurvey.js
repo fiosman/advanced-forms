1b3b757c404084b90b39da832ed9647727a40162
fiosman <fares.osman@outlook.com>
2022-02-22 00:38:53 -0500

    properly adds radio input values

--- aa-forms/src/pages/SensoryPreferencesSurvey.js
+++ aa-forms/src/pages/SensoryPreferencesSurvey.js
@@ -4,6 +4,23 @@ import spi from "../_assets/spi.json";
 
 const SensoryPreferencesSurvey = () => {
   const questions = spi.questions.slice(1);
+  const [response, setResponse] = useState({
+    0: { res: "" },
+    1: { res: "" },
+    2: { res: "" },
+    3: { res: "" },
+    4: { res: "" },
+    5: { res: "" },
+    6: { res: "" },
+    7: { res: "" },
+    8: { res: "" },
+    9: { res: "" },
+    10: { res: "" },
+    11: { res: "" },
+    12: { res: "" },
+    13: { res: "" },
+    14: { res: "" },
+  });
 
   const buildQuestions = () => {
     return (
@@ -12,26 +29,42 @@ const SensoryPreferencesSurvey = () => {
           return (
             <li key={idx}>
               <p>{question.stem}</p>
-              <label>
-                <input type="radio" value="strongly-agree" name="strongly-agree" />
-                Strongly Agree
-              </label>
-              <label>
-                <input type="radio" value="agree" name="agree" />
-                Agree
-              </label>
-              <label>
-                <input type="radio" value="neutral" name="neutral" />
-                Netural
-              </label>
-              <label>
-                <input type="radio" value="disagree" name="disagree" />
-                Disagree
-              </label>
-              <label>
-                <input type="radio" value="strongly-disagree" name="strongly-disagree" />
-                Strongly Disagree
-              </label>
+              <input
+                type="radio"
+                value="strongly-agree"
+                onChange={() => setResponse({ ...response, [idx]: { res: "strongly-agree" } })}
+                checked={response[idx].res === "strongly-agree"}
+              />
+              <label>Strongly Agree</label>
+              <input
+                type="radio"
+                value="agree"
+                onChange={() => setResponse({ ...response, [idx]: { res: "agree" } })}
+                checked={response[idx].res === "agree"}
+              />
+              <label>Agree</label>
+
+              <input
+                type="radio"
+                value="neutral"
+                onChange={() => setResponse({ ...response, [idx]: { res: "neutral" } })}
+                checked={response[idx].res === "neutral"}
+              />
+              <label>Netural</label>
+              <input
+                type="radio"
+                value="disagree"
+                onChange={() => setResponse({ ...response, [idx]: { res: "disagree" } })}
+                checked={response[idx].res === "disagree"}
+              />
+              <label>Disagree</label>
+              <input
+                type="radio"
+                value="strongly-disagree"
+                onChange={() => setResponse({ ...response, [idx]: { res: "strongly-disagree" } })}
+                checked={response[idx].res === "strongly-disagree"}
+              />
+              <label>Strongly Disagree</label>
             </li>
           );
         })}
